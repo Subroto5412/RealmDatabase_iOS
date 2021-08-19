5ff240a708a2eff54e709ae3a5f34df6efcbc03c
Subroto <subroto_mohonto>
2021-08-19 09:58:17 +0600

    demo commit

---
+++ RealmDatabase/RealmTVTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RealmTVTableViewCell.swift
+//  RealmDatabase
+//
+//  Created by masco bazar on 13/7/21.
+//
+
+import UIKit
+
+class RealmTVTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RealmDatabase/realmModel.swift
@@ -0,0 +1,12 @@
+//
+//  realmModel.swift
+//  RealmDatabase
+//
+//  Created by masco bazar on 13/7/21.
+//
+
+import UIKit
+
+class realmModel: NSObject {
+
+}
